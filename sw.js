64418cb07729e49f69b9ab05b72c7873c0e9dc00
Jonas Klemm <jonas.klemm2@gmail.com>
2026-02-05 17:19:12 +0100

    anpassen der Dateien für die PWA-Integration, Hinzufügen eines Ladebildschirms und eines Start-Buttons, sowie Aktualisierung der Service Worker Registrierung und Cache-Strategie.

--- sw.js
+++ sw.js
@@ -1,8 +1,12 @@
 const CACHE_NAME = 'ar-cache-v3';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/assets/duck.glb', // Dein Modell hier eintragen!
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './assets/duck.glb',
+  './assets/icon-192.png',
+  './assets/icon-512.png',
   'https://aframe.io/releases/1.3.0/aframe.min.js',
   'https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js'
 ];
